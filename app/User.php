848d30d80d7aef413fdf942eb795461e4884cd91
diana <dyc3@njit.edu>
2018-08-02 20:50:37 -0400

    user

--- app/User.php
+++ app/User.php
@@ -35,7 +35,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+         'email', 'password',
     ];
 
     /**
@@ -46,4 +46,17 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    public function profile()
+    {
+        return $this->hasOne('App\Profile');
+    }
+
+    public function questions()
+    {
+        return $this->hasMany('App\Question');
+    }
+    public function answers()
+    {
+        return $this->hasMany('App\Answer');
+    }
 }
