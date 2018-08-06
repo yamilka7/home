1fc18a44336607f0b1585fa37966d2708adbd1af
diana <dyc3@njit.edu>
2018-08-06 13:33:51 -0400

    edit and update profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -77,9 +77,13 @@ class ProfileController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($user, $profile)
     {
-        //
+        $user = User::find($user);
+        $profile = $user->profile;
+        $edit = TRUE;
+
+        return view('profileForm', ['profile' => $profile, 'edit' => $edit ]);
     }
 
     /**
@@ -89,10 +93,26 @@ class ProfileController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request,  $user, $profile)
     {
-        //
+        $input = $request->validate([
+            'fname' => 'required',
+            'lname' => 'required',
+        ], [
+
+            'fname.required' => ' First is required',
+            'lname.required' => ' Last is required',
+
+        ]);
+        $profile = Profile::find($profile);
+        $profile->fname = $request->lname;
+        $profile->lname = $request->lname;
+        $profile->body = $request->body;
+        $profile->save();
+
+        return redirect()->route('home')->with('message', 'Updated Profile');
     }
+
 
     /**
      * Remove the specified resource from storage.
