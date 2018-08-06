ea8ad7c5c5131cc4e3670ad555d2a70b5c4ed1d0
diana <dyc3@njit.edu>
2018-08-06 15:09:49 -0400

    create and store questions

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -25,7 +25,11 @@ class QuestionController extends Controller
      */
     public function create()
     {
-        //
+
+        $question = new Question;
+        $edit = FALSE;
+        return view('questionForm', ['question' => $question,'edit' => $edit  ]);
+
     }
 
     /**
@@ -36,7 +40,26 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $input = $request->validate([
+            'body' => 'required|min:5',
+        ], [
+
+            'body.required' => 'Body is required',
+            'body.min' => 'Body must be at least 5 characters',
+
+        ]);
+        $input = request()->all();
+
+        $question = new Question($input);
+        $question->user()->associate(Auth::user());
+        $question->save();
+
+        return redirect()->route('home')->with('message', 'IT WORKS!');
+
+
+
+        // return redirect()->route('questions.show', ['id' => $question->id]);
+
     }
 
     /**
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,7 +37,9 @@
                                         </div>
                                     </div>
                                 @empty
+                                    <a class="btn btn-primary" href="{{ route('questions.create') }}">
                                     There are no questions to view, you can  create a question.
+                                    </a>
                                 @endforelse
 
 
